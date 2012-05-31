794ec4ed12f438cf569927c7a67eec67978c3cd1
Tom Hoeck <tomhoeck@gmail.com>
2012-05-30 23:32:00 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SportsumeTom::Application.config.secret_token = 'cd30a094d09fdbd82c43e1e2042201b74614fc1a42e12fc6e0371359e8a95bb4d72396120a7dc585c7dc6e2c53ddf88e6659141e72cd4782e726740007fcbb2a'
